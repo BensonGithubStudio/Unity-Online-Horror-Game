ecd04897fdb3540bb2a93dd996719453e9114720
LAPTOP-Q8T1I111\Benson <bensonletterbox@gmail.com>
2023-01-27 11:56:23 +0800

    場景優化： 讓場景的各種音效產生隨機變化的效果 增加血跡貼圖

---
+++ Assets/Script/Effect/SoundEffect.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour
+{
+    public AudioSource SoundAudioSource;
+    public float Time1;
+    public float Time2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundAudioSource = this.gameObject.GetComponent<AudioSource>();
+
+        float a = Random.Range(Time1, Time2);
+        Invoke("PlaySound", a);
+    }
+
+    void PlaySound()
+    {
+        SoundAudioSource.Play();
+
+        float a = Random.Range(Time1, Time2);
+        Invoke("PlaySound", a);
+    }
+}
