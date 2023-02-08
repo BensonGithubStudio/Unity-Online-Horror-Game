a77cc68e5a15b62fdb7d210c2bf3fc97125f275e
LAPTOP-Q8T1I111\Benson <bensonletterbox@gmail.com>
2023-02-08 22:57:45 +0800

    物件及動畫調整： 刪除Player 1的所有物件 修改角色死亡時，槍支動畫無法呈現的錯誤 修改角色死亡時，角色無法顯現的錯誤

--- Assets/Script/PlayerAnimationControl.cs
+++ Assets/Script/PlayerAnimationControl.cs
@@ -9,6 +9,9 @@ public class PlayerAnimationControl : MonoBehaviour
     public PhotonView _pv;
     public bool IsAddingBullet;
 
+    [Header("¨¤¦â¥»¨­")]
+    public GameObject[] PlayerMeshRender;
+
     // Update is called once per frame
     void Update()
     {
@@ -34,6 +37,19 @@ public class PlayerAnimationControl : MonoBehaviour
 
             if(GameObject.Find("Game Control").GetComponent<LifeControl>().NowLife <= 0)
             {
+                _pv.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                _pv.gameObject.GetComponent<BoxCollider>().enabled = false;
+                _pv.gameObject.transform.Find("Body").transform.Find("Player Gun").transform.localEulerAngles = new Vector3(0, 0, 0);
+                foreach (GameObject m in PlayerMeshRender)
+                {
+                    if (m.GetComponent<MeshRenderer>() != null)
+                    {
+                        m.GetComponent<MeshRenderer>().enabled = true;
+                    }
+                }
+
+                _pv.gameObject.transform.position = new Vector3(_pv.gameObject.transform.position.x, 1.73f, _pv.gameObject.transform.position.z);
+                _pv.gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
                 PlayerAnimator.SetBool("IsDead", true);
             }
 
